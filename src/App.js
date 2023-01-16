7240ab907e553a5386b706e20c5b7431472f65a3
Your Name <you@example.com>
2023-01-16 19:53:31 +0530

    newcommit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItems from "./component/ExpenseItems";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/ExpenseItems.js
@@ -0,0 +1,16 @@
+function ExpenseItems(){
+    return(
+        <div>
+            <h2>expense items!</h2>
+            <div>
+                <h2>food</h2>
+                <div>500 Rs</div>
+                <h2>petrol</h2>
+                <div>505 Rs</div>
+                <h2>movies</h2>
+                <div>400 Rs</div>
+            </div>
+        </div>
+    )
+}
+export default ExpenseItems
